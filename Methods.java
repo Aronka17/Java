87af02703330be6657433c94ef599f624975fe39
Aronka17 <75635765+Aronka17@users.noreply.github.com>
2021-01-22 10:26:42 +0100

    J

---
+++ Methods.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package methods;
+
+/**
+ *
+ * @author Áron
+ */
+public class Methods {
+
+    /**
+     * @param args the command line arguments
+     */static void tagolas(){
+        System.out.println("\n--------------------------------------------------------------------------------\n");
+    }
+
+    public static void main(String[] args) {
+        tagolas();
+        System.out.println("A program neve");
+        tagolas();
+    }
+    
+}
+    
+        
+    
+    
+
